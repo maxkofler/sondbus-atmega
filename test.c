c0e4684aeb23813c2e2da83065932808d7b28b1f
Max Kofler <kofler.max.dev@gmail.com>
2024-12-07 18:56:56 +0100

    Extend C implementation by USART initialization

--- test.c
+++ test.c
@@ -1,4 +1,12 @@
+#define F_CPU 16000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 #include <stdint.h>
+
+#define USART_BAUDRATE 1000000 // We really want 2000000, that is accomplished by using 2x later
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
 typedef struct __attribute__((__packed__))
 {
@@ -21,29 +29,60 @@ void sondbus_init(sondbus_instance *state, uint8_t my_address)
     state->my_address = my_address;
 }
 
-void sondbus_rx(sondbus_instance *memory, uint8_t incoming_byte);
+uint16_t sondbus_rx(sondbus_instance *, uint8_t incoming_byte);
+
+sondbus_instance sondbus_0;
 
 int main()
 {
-    sondbus_instance state;
 
-    sondbus_init(&state, 0x1);
+    sondbus_init(&sondbus_0, 0x1);
+
+    // Enable USART x2 frequency to reach 2_000_000 BAUD
+    UCSR0A |= (1 << 1);
+    // Enable RX and TX and their respective interrupts
+    UCSR0B = (1 << 7) | (1 << 6) | (1 << 4) | (1 << 3);
+    // We operate in asynchronous mode with 8n1
+    UCSR0C = (1 << 1) | (1 << 2);
+    UBRR0L = BAUD_PRESCALE;
+    UBRR0H = (BAUD_PRESCALE >> 8);
+
+    DDRB = 0xFF;
+    PORTB = 0x0;
+
+    sei();
 
     while (1)
     {
-        // Enter the start byte: 0x55
-        sondbus_rx(&state, 0x55);
+    }
+}
 
-        // Then the type
-        sondbus_rx(&state, 0x10);
+ISR(USART0_RX_vect)
+{
+    if (UCSR0A & 1 << 7)
+    {
+        uint8_t data = UDR0;
+        uint16_t res = sondbus_rx(&sondbus_0, data);
 
-        // Address
-        sondbus_rx(&state, 0x11);
+        if ((res >> 8) & 0xFF)
+        {
+            PORTB ^= (1 << 7);
+            UDR0 = res & 0xFF;
+        }
+    }
+}
 
-        // Length
-        sondbus_rx(&state, 0x12);
+ISR(USART0_TX_vect)
+{
+    if (UCSR0A & (1 << 5))
+    {
 
-        // CRC
-        sondbus_rx(&state, 0x00);
+        uint16_t res = sondbus_rx(&sondbus_0, 0);
+
+        if ((res >> 8) & 0xFF)
+        {
+            PORTB ^= (1 << 7);
+            UDR0 = res & 0xFF;
+        }
     }
 }
